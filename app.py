cffe5ea1fb85666141afd5d7437fa3594a217bfa
Harish S.G <harishsg99@gmail.com>
2021-05-31 13:21:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import re
 import os
 from twilio.rest import Client
 import pyrebase
+from W13SCAN.api import Scanner
 bot = telebot.TeleBot("Replace this with telegram bot father key", parse_mode=None)
 
 
@@ -18,9 +19,12 @@ def echo_all(message):
     global q
     chatt = message.chat.id
     if(b[0] == "/start"):
-        bot.reply_to(message, "Welcome to tele2WA bot")
-    elif(b[0] == "/setsid"):
+        bot.reply_to(message, "Welcome to is_vulnerable bot ")
+    elif(b[0] == "/website"):
         x = b[1]
+        scanner = Scanner(threads=20)
+        scanner.put("http://example.com/?post=1")
+        scanner.run()
         print(x)
         bot.reply_to(message, "SID added")
    
