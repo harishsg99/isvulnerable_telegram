a5adcd643adf335c7a75e6a401364e88ce7d1cd4
Harish S.G <harishsg99@gmail.com>
2021-05-31 13:06:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import telebot
+import requests
+import re
+import os
+from twilio.rest import Client
+import pyrebase
+bot = telebot.TeleBot("Replace this with telegram bot father key", parse_mode=None)
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    a = message.text
+    b = a.split('@')
+    print(b)
+    global x
+    global y
+    global z
+    global q
+    chatt = message.chat.id
+    if(b[0] == "/start"):
+        bot.reply_to(message, "Welcome to tele2WA bot")
+    elif(b[0] == "/setsid"):
+        x = b[1]
+        print(x)
+        bot.reply_to(message, "SID added")
+   
+bot.polling()
